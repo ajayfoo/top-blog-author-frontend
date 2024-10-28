0b28218af81e6158d8a4479a824aa7b9fbe29283
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2024-10-28 10:19:58 +0530

    feat: slice title and body in post preview

--- src/components/PostPreview/index.jsx
+++ src/components/PostPreview/index.jsx
@@ -5,13 +5,25 @@ import { format } from "date-fns";
 
 function PostPreview({ post }) {
   const updatedAt = format(post.updatedAt, "d MMM yyyy");
+  const title = (
+    <>
+      {post.title.slice(0, 20)}
+      {post.title.length > 20 && <>&hellip;</>}
+    </>
+  );
+  const body = (
+    <>
+      {post.body.slice(0, 20)}
+      {post.body.length > 20 && <>&hellip;</>}
+    </>
+  );
   return (
     <Link className={classes["post-link"]} to={"/posts/" + post.id}>
       <article className={classes["post-preview"]}>
         <p>{updatedAt}</p>
         <div className={classes.content}>
-          <h2 className={classes.title}>{post.title}</h2>
-          <p className={classes.body}>{post.body}</p>
+          <h2 className={classes.title}>{title}</h2>
+          <p className={classes.body}>{body}</p>
         </div>
         <p className={classes.author}>{post.author.username}</p>
       </article>
