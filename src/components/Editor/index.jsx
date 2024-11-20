a407a83658337fafd6e741b3b7a6adccc292cc9d
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2024-11-20 23:54:08 +0530

    feat: complete feat to insert link in post

--- src/components/Editor/index.jsx
+++ src/components/Editor/index.jsx
@@ -1,11 +1,91 @@
 import Quill from "quill/core";
 import "./style.css";
 import classes from "./style.module.css";
-import { forwardRef, useEffect, useRef } from "react";
+import { forwardRef, useEffect, useRef, useState } from "react";
 import "./Blots";
 import SupportedBlots from "./Blots/supportedBlots.js";
+import PropTypes from "prop-types";
+
+const AddLinkModal = ({
+  show,
+  onClose,
+  onSubmit,
+  linkText,
+  onLinkTextChange,
+  link,
+  onLinkChange,
+}) => {
+  const ref = useRef(null);
+  useEffect(() => {
+    const modal = ref.current;
+    if (!show) {
+      modal.close();
+      return;
+    }
+    modal.showModal();
+  }, [show]);
+
+  const linkTextFieldId = "add-link-modal-link-text-field";
+  const linkFieldId = "add-link-modal-link-field";
+
+  return (
+    <dialog onClose={onClose} ref={ref}>
+      <div className={classes.field}>
+        <label htmlFor={linkTextFieldId}>Text</label>
+        <input
+          type="text"
+          id={linkTextFieldId}
+          value={linkText}
+          onChange={onLinkTextChange}
+        />
+      </div>
+      <div className={classes.field}>
+        <label htmlFor={linkFieldId}>Full URL</label>
+        <input
+          type="text"
+          id={linkFieldId}
+          value={link}
+          onChange={onLinkChange}
+        />
+      </div>
+      <button type="button" onClick={onClose}>
+        Close
+      </button>
+      <button type="button" onClick={onSubmit}>
+        Submit
+      </button>
+    </dialog>
+  );
+};
+
+AddLinkModal.propTypes = {
+  show: PropTypes.bool,
+  onClose: PropTypes.func,
+  onSubmit: PropTypes.func,
+  linkText: PropTypes.string,
+  onLinkTextChange: PropTypes.func,
+  link: PropTypes.string,
+  onLinkChange: PropTypes.func,
+};
 
 const Toolbar = ({ quillRef }) => {
+  const [linkText, setLinkText] = useState("");
+  const [link, setLink] = useState("");
+  const [showAddLinkModal, setShowAddLinkModal] = useState(false);
+  const handleLinkTextChange = (e) => {
+    setLinkText(e.target.value);
+  };
+  const handleLinkChange = (e) => {
+    setLink(e.target.value);
+  };
+  const showLinkModal = () => {
+    setShowAddLinkModal(true);
+  };
+  const handleCloseModal = () => {
+    setShowAddLinkModal(false);
+    setLinkText("");
+    setLink("");
+  };
   const toggleBold = () => {
     const currentFormat = quillRef.current.getFormat();
     quillRef.current.format(
@@ -44,14 +124,16 @@ const Toolbar = ({ quillRef }) => {
     );
     quillRef.current.setSelection(range.index + 2, Quill.sources.SILENT);
   };
-  const insertLink = () => {
-    const text = "I am a link";
-    const url = "https://www.archlinux.org";
+  const handleAddLinkModalSubmit = () => {
     const index = quillRef.current.getSelection(true).index;
-    quillRef.current.insertText(index, text, Quill.sources.USER);
-    quillRef.current.setSelection(index, text.length, Quill.sources.USER);
-    quillRef.current.format(SupportedBlots.LINK, url);
-    quillRef.current.setSelection(index + text.length, Quill.sources.SILENT);
+    quillRef.current.insertText(index, linkText, Quill.sources.USER);
+    quillRef.current.setSelection(index, linkText.length, Quill.sources.USER);
+    quillRef.current.format(SupportedBlots.LINK, link);
+    quillRef.current.setSelection(
+      index + linkText.length,
+      Quill.sources.SILENT
+    );
+    handleCloseModal();
   };
 
   return (
@@ -74,14 +156,27 @@ const Toolbar = ({ quillRef }) => {
       <button onClick={insertDivider} type="button">
         Divider
       </button>
-      <button onClick={insertLink} type="button">
+      <button onClick={showLinkModal} type="button">
         Link
       </button>
+      <AddLinkModal
+        show={showAddLinkModal}
+        onClose={handleCloseModal}
+        onSubmit={handleAddLinkModalSubmit}
+        link={link}
+        onLinkChange={handleLinkChange}
+        linkText={linkText}
+        onLinkTextChange={handleLinkTextChange}
+      />
     </div>
   );
 };
 
-const Editor = forwardRef(({ labelledBy }, ref) => {
+Toolbar.propTypes = {
+  quillRef: PropTypes.object,
+};
+
+const Editor = forwardRef(function Editor({ labelledBy }, ref) {
   const containerRef = useRef(null);
   useEffect(() => {
     const container = containerRef.current;
@@ -96,16 +191,22 @@ const Editor = forwardRef(({ labelledBy }, ref) => {
     };
   }, [ref]);
   return (
-    <div className="editor">
-      <Toolbar quillRef={ref} />
-      <div
-        className={classes.container}
-        aria-labelledby={labelledBy}
-        ref={containerRef}
-      ></div>
-    </div>
+    <>
+      <div className="editor">
+        <Toolbar quillRef={ref} />
+        <div
+          className={classes.container}
+          aria-labelledby={labelledBy}
+          ref={containerRef}
+        ></div>
+      </div>
+    </>
   );
 });
+
+Editor.propTypes = {
+  labelledBy: PropTypes.string,
+};
 
 Editor.displayName = "Editor";
 
