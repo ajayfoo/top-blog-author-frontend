add38cdcc04c0461939e752ad8a3faa1d829b368
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2025-08-23 19:51:02 +0530

    feat(main-nav): add links to main nav

--- src/components/MainNav/index.jsx
+++ src/components/MainNav/index.jsx
@@ -1,5 +1,29 @@
+import { NavLink } from "react-router-dom";
+import classes from "./style.module.css";
+import HomeIcon from "../Icons/HomeIcon.jsx";
+import PersonIcon from "../Icons/PersonIcon.jsx";
+
 function MainNav() {
-  return <nav>main nav</nav>;
+  return (
+    <nav className={classes["main-nav"]}>
+      <div className={classes["nav-items"]}>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? classes.active : "")}
+        >
+          <HomeIcon className={classes.icon} />
+          Home
+        </NavLink>
+        <NavLink
+          to="/account"
+          className={({ isActive }) => (isActive ? classes.active : "")}
+        >
+          <PersonIcon className={classes.icon} />
+          Account
+        </NavLink>
+      </div>
+    </nav>
+  );
 }
 
 export default MainNav;
