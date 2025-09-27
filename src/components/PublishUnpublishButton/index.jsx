5ace3b685ac91a7633a8516ffdb2d154f9d64ba7
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2025-09-27 17:27:08 +0530

    feat: show error modal on post publish/unpublish fail

--- src/components/PublishUnpublishButton/index.jsx
+++ src/components/PublishUnpublishButton/index.jsx
@@ -3,15 +3,37 @@ import { useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import Spinner from "./components/Spinner";
 import classes from "./style.module.css";
+import { useEffect } from "react";
+import { useRef } from "react";
+import ErrorModal from "../ErrorModal";
 
 const PublishUnpublishButton = ({ post }) => {
   const { replacePost } = useOutletContext();
   const [loading, setIsLoading] = useState(false);
+  const [submitError, setSubmitError] = useState(null);
+  const errorModalRef = useRef(null);
   const buttonLabel = post.isHidden ? "Publish" : "Unpublish";
+
+  useEffect(() => {
+    if (!submitError) return;
+    const modal = errorModalRef.current;
+    modal.showModal();
+    return () => {
+      modal.close();
+    };
+  }, [submitError]);
+
   const handleClick = async (e) => {
     e.preventDefault();
     await toggleIsHidden();
   };
+
+  const handleErrorModalClose = (e) => {
+    errorModalRef.current.close();
+    e.preventDefault();
+    setSubmitError(null);
+  };
+
   const toggleIsHidden = async () => {
     const auth = localStorage.getItem("auth");
     const url =
@@ -31,22 +53,36 @@ const PublishUnpublishButton = ({ post }) => {
         const updatedPost = structuredClone(post);
         updatedPost.isHidden = !updatedPost.isHidden;
         replacePost(updatedPost);
+      } else {
+        throw new Error("Something went wrong");
       }
     } catch (err) {
       console.error(err);
+      setSubmitError(
+        `Failed to ${post.isHidden ? "publish" : "unpublish"} post`
+      );
     } finally {
       setIsLoading(false);
     }
   };
   return (
-    <button
-      className={loading ? classes["button-with-spinner"] : classes["button"]}
-      disabled={loading}
-      onClick={handleClick}
-      type="button"
-    >
-      {loading ? <Spinner /> : buttonLabel}
-    </button>
+    <>
+      <button
+        className={loading ? classes["button-with-spinner"] : classes["button"]}
+        disabled={loading}
+        onClick={handleClick}
+        type="button"
+      >
+        {loading ? <Spinner /> : buttonLabel}
+      </button>
+      {submitError && (
+        <ErrorModal
+          message={submitError}
+          onClose={handleErrorModalClose}
+          ref={errorModalRef}
+        />
+      )}
+    </>
   );
 };
 
